fb419f650b4fc58350defd00d87784522feaed60
Tyler <tylerthomasallen@gmail.com>
2018-10-27 17:03:57 -0700

    Render player sprite to screen

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import playerMove from './player_move.png';
 
 class Player extends React.Component {
     render() {
+        const { position } = this.props;
         return (
-            <div>Player</div>
+            <div 
+                style={{
+                    position: "relative",
+                    top: position[1],
+                    left: position[0],
+                    backgroundImage: `url(${playerMove})`,
+                    backgroundPosition: '0 0',
+                    width: '40px',
+                    height: '40px'
+                }}
+            
+            />
         );
     }
 }
 
-export default Player;
+const mapStateToProps = ( { player })  => {
+    return {
+        ...player
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    null
+)(Player);
