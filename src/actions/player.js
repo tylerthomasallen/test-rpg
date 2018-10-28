62a46c1937bcbb67777f9851bdd8639a857a8e22
Tyler <tylerthomasallen@gmail.com>
2018-10-28 15:05:27 -0700

    Refractor player reducer and give boundaries to map

--- src/actions/player.js
+++ src/actions/player.js
@@ -1,9 +1,33 @@
+import { MAP_WIDTH, MAP_HEIGHT, SPRITE_SIZE } from '../config/constants';
+import { UP, DOWN, RIGHT, LEFT } from '../components/player';
+
 export const MOVE_PLAYER = 'MOVE_PLAYER';
 
 export const movePlayer = (direction, position) => {
     return {
         type: MOVE_PLAYER,
-        direction,
-        position
+        position: observeBoundaries(getNewPosition(direction, position), position)
     };
 };
+
+const getNewPosition = (direction, position) => {
+    switch(direction) {
+        case UP:
+            return [position[0], position[1] - SPRITE_SIZE]
+        case DOWN:
+            return [position[0], position[1] + SPRITE_SIZE]
+        case RIGHT:
+            return [position[0] + SPRITE_SIZE, position[1]]
+        case LEFT:
+            return [position[0] - SPRITE_SIZE, position[1]]
+        default:
+            return position
+    }
+}
+
+
+const observeBoundaries = (newPos, oldPos) => {
+    return (newPos[0] >= 0 && newPos[0] <= MAP_WIDTH) &&
+        (newPos[1] >= 0 && newPos[1] <= MAP_HEIGHT) ?
+        newPos : oldPos
+}
--- src/reducers/player/player_reducer.js
+++ src/reducers/player/player_reducer.js
@@ -1,7 +1,4 @@
-import { SPRITE_SIZE } from '../../config/constants';
 import { MOVE_PLAYER } from '../../actions/player';
-import { UP, DOWN, LEFT, RIGHT } from '../../components/player';
-
 
 const initialPlayer = {
     position: [0, 0]
@@ -9,24 +6,11 @@ const initialPlayer = {
 
 
 const playerReducer = (state = initialPlayer, action) => {
-
-    const { position } = action;
-
+    
+    Object.freeze(state);
     switch(action.type) {
         case MOVE_PLAYER:
-            Object.freeze(state);
-            switch(action.direction) {
-                case UP:
-                    return { ...state, position: [position[0], position[1] - SPRITE_SIZE]}
-                case DOWN:
-                    return { ...state, position: [position[0], position[1] + SPRITE_SIZE]}
-                case RIGHT:
-                    return { ...state, position: [position[0] + SPRITE_SIZE, position[1]]}
-                case LEFT:
-                    return { ...state, position: [position[0] - SPRITE_SIZE, position[1]]}
-                default:
-                    return state;
-            }
+            return {...state, position: action.position}
         default:
             return state
     }
